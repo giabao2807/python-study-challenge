2f219edbb893398dcfa563dee51a39d216118dd0
GiaBaone <102190252@sv1.dut.udn.vn>
2021-09-07 21:45:18 +0700

    sublime3 not working with input in python 7.9.21

---
+++ w3school-bonus/set.py
@@ -0,0 +1,12 @@
+'''
+	lưu trữ không theo thứ tự, k thể thay đổi, dữ liệu không trùng nhau
+	có thể chưa nhiều kiểu dữ liệu
+'''
+thisset = {"apple", "banana", "cherry","apple"}
+print(thisset)
+print(len(thisset))
+
+set1 = {"abc", 34, True, 40, "male"}
+print(set1)
+print(type(set1)) #class 'set'
+
