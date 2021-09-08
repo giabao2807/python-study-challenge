5759e8369cb0a5a7f562202661a5629466af9201
GiaBaone <102190252@sv1.dut.udn.vn>
2021-09-08 22:52:08 +0700

    half of excersice trader-trans 8.9.21

--- exercises/trader-transaction/app.py
+++ exercises/trader-transaction/app.py
@@ -4,12 +4,12 @@ from Transaction import *
 trades = [ Trader("Raoul","Cambridge"),Trader("Mario","Milan"), 
 		Trader("Alan","Cambridge"), Trader("Brian", "Cambridge")]
 
-transactions =[Transaction(trades[3],2011,500),
+transactions =[Transaction(trades[3],2011,200),
 				Transaction(trades[0],2012,1000),
 				Transaction(trades[0],2011,400),
 				Transaction(trades[1],2012,710),
 				Transaction(trades[1],2012,700),
-				Transaction(trades[2],2012,950)]
+				Transaction(trades[2],2012,250)]
 
 ''' input done
 for a in trades:
@@ -19,6 +19,9 @@ print("==========")
 for b in transactions:
 	print(b)
 '''
+def printlist(listofO:list):
+	for x in listofO:
+		print(x)
 
 
 #output:
@@ -26,8 +29,31 @@ if __name__ == '__main__':
 	#1. Find all transactions in the year 2011 and sort them by value (small to high).
 	cau1=list(filter(lambda p : p.year==2011,transactions))
 	cau1.sort(key = lambda p: p.value)
-	for a in cau1 :
-		print(a)
+	printlist(cau1)
 
+	print("======================")
 	#2. Find all transactions have value greater than 300 and sort them by trader’s city
-	cau2=
+	cau2 = list(filter(lambda p : p.value>300,transactions))
+	cau2.sort(key = lambda p: p.trader.city)
+	printlist(cau2)
+
+	print("======================")
+	#3. What are all the unique cities where the traders work?
+	cau3 = set(map(lambda p: p.trader.city,transactions))
+	printlist(cau3)
+
+	print("======================")
+	#4. Find all traders from Cambridge and sort them by name desc.
+	cau4 =list(filter(lambda p:p.city== 'Cambridge',set(map(lambda p : p.trader,transactions))))
+	cau4.sort(key = lambda p : p.name)
+	printlist(cau4)
+
+	print("======================")
+	#5. Return a string of all traders’ names sorted alphabetically.
+	cau5 = list(map(lambda p:p.name,set(map(lambda p : p.trader,transactions))))
+	cau5.sort(key = lambda p: p)
+	printlist(cau5)
+
+
+
+
